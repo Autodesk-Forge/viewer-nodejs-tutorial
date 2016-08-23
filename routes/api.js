e3f8e3cae33b599aca24c28bcb954318cc2b9ac5
Jaime Rosales <jaime.rosales.duque@autodesk.com>
2016-08-23 00:48:29 -0700

    server files

--- routes/api.js
+++ routes/api.js
@@ -16,23 +16,4 @@
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////////////////
 
-var express = require('express');
-var request = require('request');
-var router = express.Router();
-
-var credentials = (require ('fs').existsSync (__dirname + '/../credentials.js') ?
-	require (__dirname + '/../credentials')
-	: (console.log ('No credentials.js file present, assuming using CONSUMERKEY & CONSUMERSECRET system variables.'), 
-    require (__dirname + '/../credentials_'))) ;
-
-router.get ('/token', function (req, res) {
-    request.post (
-        credentials.Authentication,
-        { form: credentials.credentials },
-        function (error, response, body) {
-            if ( !error && response.statusCode == 200 )
-                res.send (body) ;
-        }) ;
-}) ;
-
-module.exports = router;
+// Check Step 2 in Server Readme.
--- server.js
+++ server.js
@@ -1,18 +1,1 @@
-var favicon = require('serve-favicon');
-var api = require('./routes/api');
-var express = require('express');
-
-var app = express();
-
-app.set('port', process.env.PORT || 3000);
-
-app.use('/', express.static(__dirname + '/www'));
-app.use(favicon(__dirname + '/www/images/favicon.ico'));
-
-// Use this route for proxying access token requests
-app.use('/api', api);
-console.log('server api ', api);
-
-var server = app.listen(app.get('port'), function() {
-    console.log('Server listening on port ' + server.address().port);
-});
+// Check Server Readme
